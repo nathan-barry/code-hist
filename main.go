757e9375190a276d21fdbc052dd7dcd71ed83f66
Nathan Barry <nathanbarry474@gmail.com>
2023-10-22 14:03:30 -0500

    grab commit info

--- main.go
+++ main.go
@@ -8,6 +8,33 @@ import (
 	"net/http"
 )
 
+func main() {
+	// Create URL
+	repoName := "nathan-barry/code-hist"
+	url := "https://api.github.com/repos/" + repoName + "/commits"
+
+	var data any
+	getJSON(url, &data)
+
+	// prettyJSON, _ := json.MarshalIndent(data, "", "    ")
+	// fmt.Println(string(prettyJSON))
+
+	var commits []Commit
+	getJSON(url, &commits)
+
+	for _, c := range commits {
+		fmt.Println(c.String())
+	}
+
+	url = "https://api.github.com/repos/" + repoName + "/commits/" + commits[0].SHA
+	var commitData any
+	getJSON(url, &commitData)
+
+	prettyJSON, _ := json.MarshalIndent(commitData, "", "    ")
+	fmt.Println(string(prettyJSON))
+
+}
+
 type Commit struct {
 	SHA string `json:"sha"`
 }
@@ -16,12 +43,8 @@ func (c Commit) String() string {
 	return c.SHA
 }
 
-func main() {
-	// Create URL
-	repoName := "nathan-barry/code-hist"
-	url := "https://api.github.com/repos/" + repoName + "/commits"
-	fmt.Println("URL:", url)
-
+func getBody(url string) []byte {
+	fmt.Println("Getting from URL:", url)
 	// Get response from URL
 	resp, err := http.Get(url)
 	if err != nil {
@@ -33,22 +56,14 @@ func main() {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
-	// Turn body into JSON
-	var data interface{}
-	if err = json.Unmarshal(body, &data); err != nil {
+	return body
+}
+
+func getJSON(url string, data any) {
+	body := getBody(url)
+	if err := json.Unmarshal(body, &data); err != nil {
 		log.Fatal(err)
-	}
-	// prettyJSON, _ := json.MarshalIndent(data, "", "    ")
-
-	var commits []Commit
-	if err = json.Unmarshal(body, &commits); err != nil {
-		log.Fatal(err)
-	}
-
-	for _, c := range commits {
-		fmt.Println(c.String())
 	}
 }
