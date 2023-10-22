e0372abda6abff142fac4b992502c2f2a715aa7c
Nathan Barry <nathanbarry474@gmail.com>
2023-10-22 11:16:07 -0500

    test serve

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Hello world!"))
+	fmt.Println("Home served")
+
+}
 
 func main() {
-	fmt.Println("Test")
+	fmt.Println("Starting Server...")
+	http.HandleFunc("/", HomeHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
