9ed87955a772314939da6d0b48700aa1cd6b74d3
Nathan Barry <nathanbarry474@gmail.com>
2023-10-22 12:49:39 -0500

    grab and print sha of commits

--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
-func HomeHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Hello world!"))
-	fmt.Println("Home served")
+type Commit struct {
+	SHA string `json:"sha"`
+}
 
+func (c Commit) String() string {
+	return c.SHA
 }
 
 func main() {
-	fmt.Println("Starting Server...")
-	http.HandleFunc("/", HomeHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Create URL
+	repoName := "nathan-barry/code-hist"
+	url := "https://api.github.com/repos/" + repoName + "/commits"
+	fmt.Println("URL:", url)
+
+	// Get response from URL
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Read body of response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Turn body into JSON
+	var data interface{}
+	if err = json.Unmarshal(body, &data); err != nil {
+		log.Fatal(err)
+	}
+	// prettyJSON, _ := json.MarshalIndent(data, "", "    ")
+
+	var commits []Commit
+	if err = json.Unmarshal(body, &commits); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, c := range commits {
+		fmt.Println(c.String())
+	}
 }
