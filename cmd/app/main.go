46c90abffa6a85b9bbe8ef030c8125e7abe1a833
Nathan Barry <nathanbarry474@gmail.com>
2023-10-24 17:24:14 -0500

    fetching code file properly and displaying it

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -17,6 +17,7 @@ func main() {
 	http.HandleFunc("/test", handlers.TestHandler)
 	http.HandleFunc("/fetch-repo", handlers.FetchRepoHandler)
 	http.HandleFunc("/fetch-files", handlers.FetchFilesHandler)
+	http.HandleFunc("/fetch-code", handlers.FetchCodeHandler)
 	http.HandleFunc("/testauth", handlers.TestAuthHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ pkg/handlers/fetch_code.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/nathan-barry/code-hist/pkg/api"
+)
+
+func FetchCodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Pinged -> FetchCode")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	githubKey := os.Getenv("GITHUB_AUTH")
+
+	url := r.FormValue("raw_url")
+	body := api.GetBody(url, githubKey)
+
+	t := template.Must(template.ParseFiles("./views/home/code.html"))
+
+	data := map[string]any{
+		"Code": string(body),
+	}
+
+	err = t.Execute(w, data)
+	if err != nil {
+		fmt.Println("Template error:", err) // Log the error
+		http.Error(w, "Could not render template", http.StatusInternalServerError)
+	}
+}
