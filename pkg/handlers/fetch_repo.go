21813ab70f05b62ace24bc04193bf053a631e564
Nathan Barry <nathanbarry474@gmail.com>
2023-10-24 11:44:02 -0500

    fetching and rendering repo commits correctly via htmx

--- pkg/handlers/fetch_repo.go
+++ pkg/handlers/fetch_repo.go
@@ -3,20 +3,38 @@ package handlers
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/nathan-barry/code-hist/pkg/api"
+	. "github.com/nathan-barry/code-hist/pkg/types"
 )
 
 func FetchRepoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Pinged -> FetchRepo")
-	fmt.Println(r.Method)
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	githubKey := os.Getenv("GITHUB_AUTH")
+
+	url := "https://api.github.com/repos/" + r.FormValue("repoURL") + "/commits"
+	var rawCommits []RawCommit
+	fmt.Println("HERE 1")
+	api.GetJSON(url, &rawCommits, githubKey)
+	fmt.Println("HERE 2")
+	api.PrintJSON("rawCommits[0]", rawCommits[0])
 
 	t := template.Must(template.ParseFiles("./views/home/commits.html"))
 
 	data := map[string]any{
-		"Method": r.Method,
+		"RawCommits": rawCommits,
 	}
 
-	err := t.Execute(w, data)
+	err = t.Execute(w, data)
 	if err != nil {
 		fmt.Println("Template error:", err) // Log the error
 		http.Error(w, "Could not render template", http.StatusInternalServerError)
